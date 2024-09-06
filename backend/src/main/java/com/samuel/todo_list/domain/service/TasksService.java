66e2bbc8034fe325f74309a422a03ffbb9f272a4
Samuel Deotti <samuel_deotti@hotmail.com>
2024-09-06 08:55:23 -0300

    adding task service

--- backend/src/main/java/com/samuel/todo_list/domain/service/TasksService.java
+++ backend/src/main/java/com/samuel/todo_list/domain/service/TasksService.java
@@ -1,5 +1,56 @@
 package com.samuel.todo_list.domain.service;
 
+import com.samuel.todo_list.domain.entities.Task;
+import com.samuel.todo_list.domain.entities.User;
+import com.samuel.todo_list.domain.repository.TaskRepository;
+import com.samuel.todo_list.domain.repository.UserRepositoy;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Slf4j
+@Service
 public class TasksService {
 
+  private final TaskRepository taskRepository;
+  private final UserRepositoy userRepositoy;
+
+  public List<Task> findAllByUser(Long userId) {
+    return taskRepository.findAllByUserId(userId);
+  }
+
+  public Task findById(Long id) {
+    return taskRepository.findById(id).orElse(null);
+  }
+
+  public Task save(Task task) {
+    User user = userRepositoy.findById(task.getUser().getId()).orElse(null);
+    return taskRepository.save(task);
+  }
+
+  public Task update(Task task) {
+    Task taskToUpdate = taskRepository.findById(task.getId()).orElse(null);
+
+    if (taskToUpdate == null) {
+      return null;
+    }
+
+    taskToUpdate.setTitle(task.getTitle());
+    taskToUpdate.setDescription(task.getDescription());
+    taskToUpdate.setStatus(task.getStatus());
+
+    return taskRepository.save(taskToUpdate);
+  }
+
+  public void deleteById(Long id) {
+    Task task = taskRepository.findById(id).orElse(null);
+
+    if (task == null) {
+      return;
+    }
+
+    taskRepository.deleteById(id);
+  }
 }
